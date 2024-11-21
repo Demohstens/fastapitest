375f5f73d632258ad357a0f570d93ebce332dc10
Demohstens <Demohstens@gmail.com>
2024-11-22 00:32:24 +0100

    azdsfdvzkjnmdsf

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
+
+from fastapi.responses import (
+    FileResponse,
+    HTMLResponse,
+    JSONResponse,
+    ORJSONResponse,
+    PlainTextResponse,
+    RedirectResponse,
+    Response,
+    StreamingResponse,
+    UJSONResponse,
+)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/@/{name}")
+async def read_item(name: str):
+    return FileResponse("./static/templates/index.html")
+
+@app.get("/register")
+async def register():
+    return FileResponse("./static/templates/index.html")
+
+@app.post("/register")
+async def register(name: Annotated[str, Form()], password: Annotated[str, Form()]):
+    print(name, password)
+    return FileResponse("./static/templates/index.html")
